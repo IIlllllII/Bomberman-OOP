89995de507593525e9855bbc6ca18f8c38bc8cd4
TORIOP23 <vinhtran23022003@gmai.com>
2022-09-30 14:07:41 +0700

    change entity

--- src/main/java/uet/oop/bomberman/components/entities/stillobjects/Brick.java
+++ src/main/java/uet/oop/bomberman/components/entities/stillobjects/Brick.java
@@ -1,11 +1,17 @@
 package uet.oop.bomberman.components.entities.stillobjects;
 
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.components.entities.Entity;
 
 public class Brick extends Entity {
-    public Brick(int i, int j, Image fxImage) {
-        super(i, j, fxImage);
+    public Brick(int i, int j) {
+        super(i, j);
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+
     }
 
     @Override
